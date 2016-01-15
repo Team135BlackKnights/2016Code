e5d30ef7afce4cb4a2965efa90b9bd1c42157f2a
team135 <robotics@phm.k12.in.us>
2016-01-15 16:25:12 -0500

    fixed issue things

--- NewBasicTemplate2016/src/OI.cpp
+++ NewBasicTemplate2016/src/OI.cpp
@@ -5,13 +5,12 @@
 
 OI::OI()
 {
-//
-	// Process operator interface input here.
-	//sticks[LEFT].reset(new Joystick(JOYSTICK_LEFT)); //Declares new Joystick
-	//sticks[RIGHT].reset(new Joystick(JOYSTICK_RIGHT));
-	//sticks[MANIPULATOR_CONTROL].reset(new Joystick(JOYSTICK_MANIPULATOR_CONTROL));
 
-	/*
+	// Process operator interface input here.
+	sticks[LEFT].reset(new Joystick(JOYSTICK_LEFT)); //Declares new Joystick
+	sticks[RIGHT].reset(new Joystick(JOYSTICK_RIGHT));
+	sticks[MANIPULATOR_CONTROL].reset(new Joystick(JOYSTICK_MANIPULATOR_CONTROL));
+
 	for (int i = 0; i < NUMBER_O_JOYSTICKS; i++) //Declares new buttons for Joysticks max # of buttons is 12
 	{
 
@@ -20,41 +19,38 @@ OI::OI()
 			buttons[i][j].reset(new JoystickButton(sticks[i].get(), j));
 		}
 	}
-	*/
 }
 
 float OI::GetStickX(int hand)
 {
-	//float value = sticks[hand]->GetX(); // Returns X axis value of Joysticks
+	float value = sticks[hand]->GetX(); // Returns X axis value of Joysticks
 	//if (abs(value) > DEAD_BAND)
-		return 0;
+		return value;
 	//return 0;
 }
 
 float OI::GetStickY(int hand)
 {
-	//float value = sticks[hand]->GetY(); // Returns Y axis value of Joysticks
+	float value = sticks[hand]->GetY(); // Returns Y axis value of Joysticks
 	//if (abs(value) > DEAD_BAND)
-		return 0;
+		return value;
 	//return 0;
 }
 
 float OI::GetStickTwist(int hand)
 {
-	//float value = sticks[hand]->GetTwist(); // Returns Twist axis value of Joysticks
+	float value = sticks[hand]->GetTwist(); // Returns Twist axis value of Joysticks
 	//if (abs(value) > DEAD_BAND)
-		return 0;
+		return value;
 	//return 0;
 }
 
 float OI::GetStickSlider(int hand)
 {
-	//return 1 - sticks[hand]->GetThrottle();
-	return 0;
+	return 1 - sticks[hand]->GetThrottle();
 }
 
 bool OI::GetButton(int stick, int button)
 {
-	//return buttons[stick][button]->Get();
-	return 0;
+	return buttons[stick][button]->Get();
 }
