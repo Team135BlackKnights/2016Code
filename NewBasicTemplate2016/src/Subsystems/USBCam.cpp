af507f14a180c9b320002c0010409fd1c03e0c72
samba_000 <samba_000@Computron>
2016-01-15 22:26:00 -0500

    Added code to run and read data from Roborio GRIP. Added findbiggest

--- NewBasicTemplate2016/src/Subsystems/USBCam.cpp
+++ NewBasicTemplate2016/src/Subsystems/USBCam.cpp
@@ -1,5 +1,10 @@
-#include "USBCam.h"
-#include "../RobotMap.h"
+#include <CameraServer.h>
+#include <ErrorBase.h>
+#include <llvm/ArrayRef.h>
+#include <Subsystems/USBCam.h>
+#include <unistd.h>
+#include <iostream>
+#include <vector>
 
 USBCam::USBCam() :
 		Subsystem("ExampleSubsystem")
@@ -9,13 +14,39 @@ USBCam::USBCam() :
 
 void USBCam::InitDefaultCommand()
 {
-	// Set the default command for a subsystem here.
-	//SetDefaultCommand(new MySpecialCommand());
-	CameraServer::GetInstance()->SetQuality(50); //set the cam quality of jpeg to be around 50 (out of 100)
-
+	//***first 2 lines primarily for the drivers, can be removed with
+	//0 impact on vision processing.***
+	//set the cam quality of jpeg to be around 50 (out of 100)
+	CameraServer::GetInstance()->SetQuality(50);
+	//Start the automatic capture to dashboard
 	CameraServer::GetInstance()->StartAutomaticCapture(CAMERA_NAME);
+
+	//starts the yung grip and checks for error
+	//error will most likely be if we dont have JRE.
+	if (execl(JAVA,"-jar", GRIP_JAR, GRIP_PROJECT, nullptr) == -1) {
+		wpi_setErrnoErrorWithContext("Failed to run GRIP"); //throws error
+	}
+
+
 	std::cout << "INITIALIZING CAMERA" << std::endl;
 }
 
-// Put methods for controlling this subsystem
-// here. Call these from Commands.
+std::tuple<double, double, double> USBCam::findBiggest()
+{
+	auto areas = grip->GetNumberArray("myCountoursReport/area", llvm::ArrayRef<double>()),
+	xs = grip->GetNumberArray("myCountoursReport/x", llvm::ArrayRef<double>()),
+	ys = grip->GetNumberArray("myCountoursReport/y", llvm::ArrayRef<double>());
+
+	double targetArea = -1.0, targetX = 0.0, targetY = 0.0;
+	for (int i = 0; i < areas.size(); i++){
+		double area = areas[i], x = xs[i], y = ys[i];
+		if (area > targetArea){
+			targetArea = area;
+			targetX = x;
+			targetY = y;
+		}
+		std::cout << "Got contour: area=" << area << ", x=" << x << ", y=" << y << std::endl;
+    }
+
+	return std::make_tuple(targetArea, targetX, targetY);
+}
--- NewBasicTemplate2016/src/Subsystems/USBCam.h
+++ NewBasicTemplate2016/src/Subsystems/USBCam.h
@@ -1,16 +1,27 @@
 #ifndef USBCam_H
 #define USBCam_H
 
-#include "Commands/Subsystem.h"
+#include <Commands/Subsystem.h>
+#include <networktables/NetworkTable.h>
+#include <memory>
 #include "WPILib.h"
+#include <unistd.h>
 
 class USBCam: public Subsystem
 {
 private:
 	const char* CAMERA_NAME = "cam2";
+	const char * const JAVA = "/usr/local/frc/JRE/bin/java";
+	const char * const GRIP_JAR = "/home/lvuser/grip.jar";
+	const char * const GRIP_PROJECT = "home/lvuser/myproject.grip";
+	std::shared_ptr<NetworkTable> grip = NetworkTable::GetTable("grip");
+	//std::vector<double> areas;
+	//std::vector<double> xs;
+	//std::vector<double> ys;
 public:
 	USBCam();
 	void InitDefaultCommand();
+	std::tuple<double, double, double> findBiggest();
 };
 
 #endif
