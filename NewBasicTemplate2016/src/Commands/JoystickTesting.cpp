4deef428e6621954e7228b8df6dde1bee0a11aef
Conner Swift <cswift33@phm.k12.in.us>
2016-01-22 18:10:28 -0500

    Created a new Command to Test out Logging Data

---
+++ NewBasicTemplate2016/src/Commands/JoystickTesting.cpp
@@ -0,0 +1,38 @@
+#include "JoystickTesting.h"
+
+JoystickTesting::JoystickTesting()
+{
+	// Use Requires() here to declare subsystem dependencies
+	// eg. Requires(chassis);
+}
+
+// Called just before this Command runs the first time
+void JoystickTesting::Initialize()
+{
+
+}
+
+// Called repeatedly when this Command is scheduled to run
+void JoystickTesting::Execute()
+{
+
+}
+
+// Make this return true when this Command no longer needs to run execute()
+bool JoystickTesting::IsFinished()
+{
+	return false;
+}
+
+// Called once after isFinished returns true
+void JoystickTesting::End()
+{
+
+}
+
+// Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+void JoystickTesting::Interrupted()
+{
+
+}
