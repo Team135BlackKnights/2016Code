1ccdb028bbebc88b52301c7012c4cdc2af6bbe7b
Kenneth Ham <cyritz@hotmail.com>
2016-01-26 16:53:41 -0500

    Joystick count fix
    Corrected 'ignore controller 0 and button 0' in array + mirrored changes to static constants

--- NewBasicTemplate2016/src/OI2.h
+++ NewBasicTemplate2016/src/OI2.h
@@ -10,15 +10,15 @@ protected:
 	static const int MAX_JOYSTICK_BUTTONS = 12; //max number of buttons
 	//unique_ptr creates a(n) unique pointer  ask Eddie if confused
 	std::unique_ptr<Joystick> joysticksArray[JOYSTICKS]; //creates an array of the joysticks
-	std::unique_ptr<JoystickButton> buttonsArray[JOYSTICKS][MAX_JOYSTICK_BUTTONS + 1]; //creates an array of the joysticks vs the buttons starting with buttons at 1
+	std::unique_ptr<JoystickButton> buttonsArray[JOYSTICKS + 1][MAX_JOYSTICK_BUTTONS + 1]; //creates an array of the joysticks vs the buttons starting with buttons at 1
 		 /*	 	 	 	 	 	 Buttons * 0 is invalid *
 		  	 Controller [0][1][2][3][4][5][6][7]...
 		  	 Controller [1]
 		  	 Controller [2]
 		 */
 public:
-	static const int LEFT = 0; // sets LEFT controller
-	static const int RIGHT = 1; // sets LEFT controller
+	static const int LEFT = 1; // sets LEFT controller
+	static const int RIGHT = 2; // sets LEFT controller
 	static constexpr float DEAD_ZONE = .05f; //declares dead zone
 
 	OI();
