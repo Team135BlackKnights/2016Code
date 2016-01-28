60911a3621d39136188527b1feb8d4c8e3a8ce17
Kenneth Ham <cyritz@hotmail.com>
2016-01-28 14:09:56 -0500

    Update OI.h

--- NewBasicTemplate2016/src/OI.h
+++ NewBasicTemplate2016/src/OI.h
@@ -8,8 +8,8 @@ class OI
 protected:
 	static const int NUMBER_O_JOYSTICKS = 3;
 	static const int MAX_JOYSTICK_BUTTONS = 12;
-	std::unique_ptr<Joystick> sticks[NUMBER_O_JOYSTICKS + 1];
-	std::unique_ptr<JoystickButton> buttons[NUMBER_O_JOYSTICKS + 1][MAX_JOYSTICK_BUTTONS + 1];
+	std::unique_ptr<Joystick> sticks[NUMBER_O_JOYSTICKS];
+	std::unique_ptr<JoystickButton> buttons[NUMBER_O_JOYSTICKS][MAX_JOYSTICK_BUTTONS + 1];
 
 public:
 	static const int LEFT = 0;
