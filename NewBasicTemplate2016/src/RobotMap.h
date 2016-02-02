6c53fde0ce88c615ef96766794a855d81d182348
team135 <robotics@phm.k12.in.us>
2016-02-02 13:28:34 -0500

    updated robotmap

--- NewBasicTemplate2016/src/RobotMap.h
+++ NewBasicTemplate2016/src/RobotMap.h
@@ -15,10 +15,10 @@
 
 	//constexpr bool ROBOT_MAIN = false;
 
-    const int MOTOR_FRONT_LEFT	= 10;
-    const int MOTOR_REAR_LEFT	= 11;
-    const int MOTOR_FRONT_RIGHT	= 12;
-    const int MOTOR_REAR_RIGHT	= 13;
+    const int MOTOR_FRONT_LEFT	= 11;
+    const int MOTOR_REAR_LEFT	= 13;
+    const int MOTOR_FRONT_RIGHT	= 10;
+    const int MOTOR_REAR_RIGHT	= 12;
 
     const int JOYSTICK_LEFT = 1;
     const int JOYSTICK_RIGHT = 0;
