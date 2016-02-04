a99f0b664f285b3373ebef8c202e338a426ceab7
cswift33 <cswift33@phm.k12.in.us>
2016-02-04 16:54:53 -0500

    New Shooter Subsystem

---
+++ NewBasicTemplate2016/src/Subsystems/Shooter.cpp
@@ -0,0 +1,24 @@
+#include "Shooter.h"
+#include "../RobotMap.h"
+
+Shooter::Shooter() :
+		PIDLogging("Shooter",)
+{
+
+	shooter.reset(new CANTalon(MOTOR_SHOOT_BOULDER));
+	motors[TWO_WHEEL_SHOOTER_MOTOR] = shooter.get();
+
+}
+
+void Shooter::InitDefaultCommand()
+{
+	// Set the default command for a subsystem here.
+	//SetDefaultCommand(new MySpecialCommand());
+}
+
+void Shooter::DriveMotor(int motorIndex, double motorPower) {
+	motors[motorIndex]->Set(motorPower);
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
---
+++ NewBasicTemplate2016/src/Subsystems/Shooter.h
@@ -0,0 +1,23 @@
+#ifndef Shooter_H
+#define Shooter_H
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+#include "PIDLogging.h"
+
+class Shooter: public PIDLogging
+{
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+
+	//  Motor for Two Wheels, motor for raising and lower arm, motor for collection of boulder
+	std::unique_ptr<CANTalon> shooter;
+	static const int TWO_WHEEL_SHOOTER_MOTOR = 0;
+public:
+	Shooter();
+	void InitDefaultCommand();
+	void DriveMotor(int, double);
+};
+
+#endif
