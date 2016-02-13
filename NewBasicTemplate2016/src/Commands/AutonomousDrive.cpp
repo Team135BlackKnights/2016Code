384f06a1f4cb040b923ea057e63837a60de97bac
ckidder85 <ckidder85@PHS140-008584.phmschools.org>
2016-02-13 11:38:12 -0500

    Commenting

--- NewBasicTemplate2016/src/Commands/AutonomousDrive.cpp
+++ NewBasicTemplate2016/src/Commands/AutonomousDrive.cpp
@@ -7,19 +7,21 @@ AutonomousDrive::AutonomousDrive(float left, float right, MODE driveMode, int ti
 	// eg. Requires(chassis);
 	Requires(driveTrain.get());
 
-	this->left = left;
+	this->left = left; //Assigns parameters to private floats
 	this->right = right;
 
-	if (driveMode == MODE::TIME){
-		timer = new Timer();
+	if (driveMode == MODE::TIME){ // Starts TIME mode
+		timer = new Timer(); // Creates a new timer
 		//this->targetTime = time;
-		this->SetTimeout(time);
+		this->SetTimeout(time); // Sets timeout to parameter time
 	}
 
-	else if (driveMode == MODE::DISTANCE){}
-	else if (driveMode == DEFENSE){}
+	else if (driveMode == MODE::DISTANCE){} // Starts DISTANCE mode
 
-	this->SetTimeout(time);
+	else if (driveMode == DEFENSE){ // Starts DEFENSE mode
+
+	}
+
 
 }
 
@@ -33,24 +35,24 @@ void AutonomousDrive::Initialize()
 void AutonomousDrive::Execute()
 {
 
-	driveTrain->DriveTank(left,right);
+	driveTrain->DriveTank(left,right); // Drives robot with private left and right values
 }
 
 // Make this return true when this Command no longer needs to run execute()
 bool AutonomousDrive::IsFinished()
 {
-	return this->IsTimedOut();
+	return this->IsTimedOut(); // Returns true and ends instance if the command has timed out
 }
 
 // Called once after isFinished returns true
 void AutonomousDrive::End()
 {
-	driveTrain->DriveTank(0.0f,0.0f);
+	driveTrain->DriveTank(0.0f,0.0f); // Stops robot movement
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void AutonomousDrive::Interrupted()
 {
-	End();
+	End(); // Calls end() if command is interrupted
 }
