ead419a755abe3ea00950dc5087d6ee2e6a870cd
ckidder85 <ckidder85@PHS140-011118.phmschools.org>
2016-02-17 14:57:06 -0500

    Missed a line

--- NewBasicTemplate2016/src/CommandBase.cpp
+++ NewBasicTemplate2016/src/CommandBase.cpp
@@ -20,9 +20,6 @@ CommandBase::CommandBase() :
 
 void CommandBase::init()
 {
-	// Create a single static instance of all of your subsystems. The following
-	// line should be repeated for each subsystem in the project.
-
 	driveTrain.reset(new DriveTrain());
 	oi.reset(new OI());
 	serialCommunication.reset(new SerialCommunication());
