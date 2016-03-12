5073284112693d6419bf679d2ac64812a780b68d
ROBOTICS <ROBOTICS@staffwless.connectify>
2016-03-12 00:26:31 -0500

    Changes to robot.cpp. Hopefully auto works now?

--- NewBasicTemplate2016/src/Robot.cpp
+++ NewBasicTemplate2016/src/Robot.cpp
@@ -1,15 +1,26 @@
 #include "WPILib.h"
 #include "Commands/Command.h"
 #include "CommandBase.h"
-#include "Commands/ChangeManipulator.h"
-#include "Commands/ChangeDriver.h"
+//#include "Commands/ChangeManipulator.h"
+//#include "Commands/ChangeDriver.h"
+#include "Commands/AutoCommand.h"
 
 class Robot: public IterativeRobot
 {
 private:
+	SendableChooser* autoChooser;
+	CommandGroup* autoCommand;
+
+
 	void RobotInit()
 	{
 		CommandBase::init();
+
+		autoChooser = new SendableChooser();
+		autoChooser->AddDefault("No Low Bar And Over Defense", new AutoCommand(Arm::AUTO_NON_LOW_BAR, true));
+		autoChooser->AddObject("Low Bar and Over Defense", new AutoCommand(Arm::AUTO_LOW_BAR, true));
+		autoChooser->AddObject("Only Reach Defense", new AutoCommand(false, false));
+		SmartDashboard::PutData("Autonomous Select", autoChooser);
 	}
 	
 	void DisabledPeriodic()
@@ -19,6 +30,8 @@ private:
 
 	void AutonomousInit()
 	{
+		autoCommand = (CommandGroup*) autoChooser->GetSelected();
+		autoCommand->Start();
 	}
 
 	void AutonomousPeriodic()
